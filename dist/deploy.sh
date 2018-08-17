53aff3347a9d54ef4b932dded1f70043080d6ed1
Jay Holtslander <jay@skunkworks.ca>
2018-08-17 09:53:07 -0700

    Add favicons

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,12 +1,30 @@
 clear &&
+
+echo rsyncing png files to \"/\" to remote host... &&
+rsync -ru dist/*.png serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
+echo rsyncing ico files to \"/\" to remote host... &&
+rsync -ru dist/*.ico serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
+echo rsyncing xml files to \"/\" to remote host... &&
+rsync -ru dist/*.xml serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
+echo rsyncing webmanifest files to \"/\" to remote host... &&
+rsync -ru dist/*.webmanifest serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
 echo rsyncing html files to \"/\" to remote host... &&
 rsync -ru dist/*.html serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
 echo rsyncing php files to \"/\" to remote host... &&
 rsync -ru dist/*.php serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
 echo rsyncing svg files to \"/\" to remote host... &&
 rsync -ru dist/*.svg serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
 echo rsyncing htaccess files to \"/\" to remote host... &&
 rsync -ru dist/.htaccess serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+
+
 
 echo rsyncing folder \"/css\" to remote host... &&
 rsync -ru dist/css/ serverpilot@159.203.12.174:apps/cf-cache-status/public/css/ --delete &&
