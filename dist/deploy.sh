26df90091fbc024ca9c5061dbd344531e6be13b2
Jay Holtslander <jay@skunkworks.ca>
2018-08-17 10:10:16 -0700

    path change in deploy script

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,33 +1,33 @@
 clear &&
 
 echo rsyncing png files to \"/\" to remote host... &&
-rsync -ru dist/*.png serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.png serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 echo rsyncing ico files to \"/\" to remote host... &&
-rsync -ru dist/*.ico serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.ico serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 echo rsyncing xml files to \"/\" to remote host... &&
-rsync -ru dist/*.xml serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.xml serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 echo rsyncing webmanifest files to \"/\" to remote host... &&
-rsync -ru dist/*.webmanifest serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.webmanifest serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 echo rsyncing html files to \"/\" to remote host... &&
-rsync -ru dist/*.html serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.html serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 echo rsyncing php files to \"/\" to remote host... &&
-rsync -ru dist/*.php serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.php serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 echo rsyncing svg files to \"/\" to remote host... &&
-rsync -ru dist/*.svg serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.svg serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 echo rsyncing htaccess files to \"/\" to remote host... &&
-rsync -ru dist/.htaccess serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
+rsync -ru *.htaccess serverpilot@159.203.12.174:apps/cf-cache-status/public/ --delete &&
 
 
 
 echo rsyncing folder \"/css\" to remote host... &&
-rsync -ru dist/css/ serverpilot@159.203.12.174:apps/cf-cache-status/public/css/ --delete &&
+rsync -ru css/ serverpilot@159.203.12.174:apps/cf-cache-status/public/css/ --delete &&
 echo rsyncing folder \"/content\" to remote host... &&
-rsync -ru dist/js/ serverpilot@159.203.12.174:apps/cf-cache-status/public/js/ --delete &&
+rsync -ru js/ serverpilot@159.203.12.174:apps/cf-cache-status/public/js/ --delete &&
 echo DONE!
