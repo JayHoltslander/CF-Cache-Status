618994ea465578139c1d2b1c26512303074614f4
Jay Holtslander <jay@skunkworks.ca>
2018-08-21 11:35:43 -0700

    Add GTM w FB Chat

--- dist/index.php
+++ dist/index.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html lang="en" >
 <head>
+	
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w,d,s,l,i){w[l]=w[l]||[];
+			w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+			var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+			j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-P9X3J7M');
+	</script>
+	<!-- End Google Tag Manager -->
+
   <meta charset="UTF-8">
   <title>CF-Cache-Status</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<!-- <link rel="stylesheet" href="css/normalize.css"> -->
-  <!-- <link rel="stylesheet" href="css/materialize.css"> -->
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
-	<!-- <link rel="stylesheet" href="css/material-icons.css"> -->
-	<link rel="stylesheet" href="css/optimized.css">
+	<link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/materialize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/material-icons.css">
+	<!-- <link rel="stylesheet" href="css/optimized.css"> -->
 
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=A07RLdrO2d">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=A07RLdrO2d">
@@ -320,4 +331,8 @@ else {
 
 
 </body>
+	<!-- Google Tag Manager -->
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9X3J7M" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	</noscript>
 </html>
