338028d308dfad867e9c9152ccc0aaa1e6a88ed3
Jay Holtslander <jay@skunkworks.ca>
2018-10-03 13:33:28 -0700

    Add Toronto explanation

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,11 @@
-$(document).ready(function(){$(".modal").modal(),$(".dropdown-button").dropdown({belowOrigin:!0}),$("ul.tabs").tabs(),$(".collapsible").collapsible(),$("#X").click(function(){console.log("reached"),$("#search").val(""),$("#url-test-bar").submit()})});
+$(document).ready(function(){
+// MODAL
+$(".modal").modal(),
+// DROPDOWNS
+$(".dropdown-button").dropdown({belowOrigin:!0}),
+// TABS
+$("ul.tabs").tabs(),
+// COLLAPSIBLE
+$(".collapsible").collapsible(),
+// CUSTOM CLEARABLE INPUT WITH REFRESH
+$("#X").click(function(){console.log("reached"),$("#search").val(""),$("#url-test-bar").submit()})});
